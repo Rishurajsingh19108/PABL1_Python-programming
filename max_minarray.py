2489d30ff090e8fcb0bbe31ea2ba38fe4958b0dd
Rishurajsingh19108 <rishurajtr45@gmail.com>
2026-01-30 12:03:12 +0530

    Implement min and max value finder in array

---
+++ max_minarray.py
@@ -0,0 +1,12 @@
+arr = [1, 4, 3, 5, 8, 6]
+
+min_val = arr[0]
+max_val = arr[0]
+
+for num in arr:
+    if num < min_val:
+        min_val = num
+    if num > max_val:
+        max_val = num
+
+print([min_val, max_val])
